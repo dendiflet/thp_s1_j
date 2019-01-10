91d7030bc1cc8941ef20a13dacd949ef80995698
dendiflet <bretin.iannis@gmail.com>
2019-01-10 22:56:45 +0100

    starway fini, reste a modifier la pyramide

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,13 +1,109 @@
-marche_actuelle = 0
-nbdd = 0
-dd = []
+$arrayresultats = []  						#sinitialisation de l'array chaque valeur est : nombre de lancés pour arriver a la marche 10
 
-10.times do
-dd <<	 rand(1..6)
+def initialisation
+$position_actuelle = 0
+$nbdd = 0
+#test
+# puts $position_actuelle
+# puts $nbdd
+end
+
+
+def jetde
+	rand(1..6)
+end
+
+
+
+
+def montee
+	$position_actuelle = $position_actuelle + 1
+	puts "													tu progresses vers le sommet !"
+end
+def descente
+	$position_actuelle = $position_actuelle - 1
+	puts "													une chance sur 6 de descendre, c'était celle la !"
+end
+def pasbouger
+	puts "													profites en pour faire une pause"
+end
+
+
+
+def affichage_position
+	puts "tu est a la marche #{$position_actuelle}"
+end
+
+
+def deplacement							#utilise montee/descente/stagnation
+	if [5,6].include?($jet)
+		montee
+	elsif [2, 3, 4].include?($jet)
+		pasbouger
+	elsif $jet == 1
+		descente
+	end
+end
+
+
+
+def jeux
+	while $position_actuelle < 10		
+		$jet = jetde
+		$nbdd = $nbdd + 1
+		deplacement	
+		affichage_position
+
+#test 
+# puts "#{$nbdd}  nombre de lancer actuel"
+# puts "#{$jet} valeur du jet de dé"
+
+	end
+end
+
+
+def resultat
+	puts"\n"
+	puts "tu as mis #{$nbdd} coups de dés pour arriver au sommet !"
+	$arrayresultats << $nbdd				#rempli l'array pour avoir une trace et faire des stats
+	puts"\n"
+end
+
+
+
+def perform
+
+	initialisation
+	jeux
+	resultat
+
+end
+
+
+
+def average_finish_time
+
+	100.times{ perform}
+	puts"\n"
+	puts "voici l'array des 100 fois"
+	puts "#{$dd} \n"
+	moyenne = ($arrayresultats.sum)/100
+	puts " la moyenne sur ces 100 lancés est de #{moyenne}"
+	puts"\n"
+	puts"\n"
+	puts"\n"
 
 
 end
-puts dd
+
+average_finish_time
+
+
+
+
+
+
+
 
 
 
--- testcour2.rb
+++
@@ -1,6 +0,0 @@
-def ex1 ()
-	i=0
-	numbers = []
-	numbers.each {|num| puts num }
-end
-ex1
