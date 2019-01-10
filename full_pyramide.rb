9c220b32c17305df81694a7a0f92db3cf0af78cd
dendiflet <bretin.iannis@gmail.com>
2019-01-10 15:05:31 +0100

    wtf et full parfaites

--- full_pyramide.rb
+++ full_pyramide.rb
@@ -1,28 +1,20 @@
+# interface utilisateur
 puts "nombre d'étages svp toutancarton (entre 1 et 25)"
 puts ">"
 a = gets.chomp.to_i
-
-def halfpyra (a)
-	1.upto(a) {|i| print " "*(a-i);puts "#"*(2*i-1) }
-end
-
-def reversepyra (a)
-	(a-1).downto(1) { |i| print " "*(a-i);puts "#"*(2*i-1)}
-end
-
+# voici la fonction
 def fullpyra (a)
-	halfpyra (a)
-	reversepyra (a)
+	1.upto(a) {|i| print " "*(a-i);puts "#"*(2*i-1) } #mieux que (1..a)
 end
-
+#vérification et impression du résultat
 if  a > 1 && a < 25
-
-fullpyra (a)
-
+	fullpyra (a)
 else
 	puts "entre 1 et 25 c'est mieux"
 end
 
+	# code fullpyra brut
+	# a = gets.chomp.to_i
 	# 1.upto(a) {|i| print " "*(a-i);puts "#"*(2*i-1) }
 	# (a-1).downto(1) { |i| print " "*(a-i);puts "#"*(2*i-1)}
 
--- wtf_pyramide.rb
+++ wtf_pyramide.rb
@@ -1,28 +1,27 @@
+# interface utilisateur
 puts "nombre d'étages svp toutancarton (entre 1 et 25)"
 puts ">"
 a = gets.chomp.to_i
-
-def halfpyra (a)
+# voici les fonctions
+def fullpyra (a)
 	1.upto(a) {|i| print " "*(a-i);puts "#"*(2*i-1) }
 end
-
-def reversepyra (a)
+def reversefullpyra (a)
 	(a-1).downto(1) { |i| print " "*(a-i);puts "#"*(2*i-1)}
 end
-
-def fullpyra (a)
-	halfpyra (a)
-	reversepyra (a)
+#la fonction de fonction
+def wtfpyra (a)
+	fullpyra (a)
+	reversefullpyra (a)
 end
-
+#vérification de la valeur saisie et impression du résultat (ou pas)
 if  a > 1 && a < 25
-
-fullpyra (a)
-
+	wtfpyra (a)
 else
 	puts "entre 1 et 25 c'est mieux"
 end
-
+	# code losange brut
+	# a = gets.chomp.to_i
 	# 1.upto(a) {|i| print " "*(a-i);puts "#"*(2*i-1) }
 	# (a-1).downto(1) { |i| print " "*(a-i);puts "#"*(2*i-1)}
 
